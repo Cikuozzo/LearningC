bcbaac3e901da05aba5f7c2c19e06ffbd537b8f8
Cikuozzo <roccorotondo@kakao.com>
2026-01-13 17:13:11 +0100

    added a new reference for if-else

---
+++ Reference/if-else/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+
+    printf("Insert a number: ");
+    scanf("%d", &x);
+
+    if(x%2 == 0){
+
+        printf("The number is even");
+
+    }else{
+
+        printf("The number is odd");
+
+    }
+
+    return 0;
+
+}
